c6f95a770c734cd8985201b4f05f90e3a2b6c4b4
Jay <jayliu@zymber.tech>
2024-05-17 16:21:42 +0800

    + Splash screen

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+/* 
+Splash screen - Expo Documentation
+https://docs.expo.dev/develop/user-interface/splash-screen/
+ */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
