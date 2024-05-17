6691829147563a25f67e823491e15cb0a7a7ffd4
Jay <jayliu@zymber.tech>
2024-05-17 16:23:24 +0800

    + Safe areas $ npx expo install react-native-safe-area-context

--- App.js
+++ App.js
@@ -1,24 +1,16 @@
 /* 
-App icons - Expo Documentation
-https://docs.expo.dev/develop/user-interface/app-icons/
+Safe areas - Expo Documentation
+https://docs.expo.dev/develop/user-interface/safe-areas/
  */
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View>
+        <Text>My App</Text>
+      </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
