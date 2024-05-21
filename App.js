dd918521363d4f585c7677b23a2f3262458bfee3
Jay <jayliu@zymber.tech>
2024-05-21 17:57:30 +0800

    <> Assets - Serve an asset remotely $ npx expo install expo-image

--- App.js
+++ App.js
@@ -2,14 +2,11 @@
 Assets - Expo Documentation
 https://docs.expo.dev/develop/user-interface/assets/
  */
-import { Image } from 'react-native';
-import { useAssets } from 'expo-asset';
+import { Image } from 'expo-image';
 
 export default function App() {
-  const [assets, error] = useAssets([
-    require('./path/to/example-1.jpg'),
-    require('./path/to/example-2.png'),
-  ]);
-
-  return assets ? <Image source={assets[0]} /> : null;
+  return (
+    // <Image source={{ uri: 'https://example.com/logo.png' }} style={{ width: 50, height: 50 }} />
+    <Image source={{ uri: 'https://reactnative.dev/img/showcase/messengerdesktop.png' }} style={{ width: 50, height: 50 }} />
+  );
 }
