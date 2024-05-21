06c3a1cfe661d21187f90c90377df0ffb201cdd8
Jay <jayliu@zymber.tech>
2024-05-21 17:00:42 +0800

    <> Fonts - Loading font async

--- App.js
+++ App.js
@@ -4,23 +4,42 @@ https://docs.expo.dev/develop/user-interface/fonts/
  */
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
 
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Inter-SemiBoldItalic': 'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
-  });
+let customFonts = {
+  'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+  'Inter-SemiBoldItalic': 'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
+};
 
-  if (!fontsLoaded) {
-    return null;
+export default class App extends React.Component {
+  state = {
+    fontsLoaded: false,
+  };
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: 'Inter-SemiBoldItalic', fontSize: 30 }}>Inter SemiBoldItalic</Text>
-      <Text style={{ fontSize: 30 }}>Platform Default</Text>
-    </View>
-  );
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return null;
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontFamily: 'Inter-Black', fontSize: 30 }}>Inter Black</Text>
+        <Text style={{ fontFamily: 'Inter-SemiBoldItalic', fontSize: 30 }}>
+          Inter SemiBoldItalic
+        </Text>
+        <Text style={{ fontSize: 30 }}>Platform Default</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
