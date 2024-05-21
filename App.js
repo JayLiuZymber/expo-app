d7f6da1b2907442da3e1a5ebfd3768afd5d478a2
Jay <jayliu@zymber.tech>
2024-05-21 17:48:46 +0800

    <> Assets - Load an asset at runtime $ npx expo install expo-asset

--- App.js
+++ App.js
@@ -3,9 +3,13 @@ Assets - Expo Documentation
 https://docs.expo.dev/develop/user-interface/assets/
  */
 import { Image } from 'react-native';
+import { useAssets } from 'expo-asset';
 
 export default function App() {
-  return (
-    <Image source={require('./assets/images/example.png')} />
-  );
+  const [assets, error] = useAssets([
+    require('./path/to/example-1.jpg'),
+    require('./path/to/example-2.png'),
+  ]);
+
+  return assets ? <Image source={assets[0]} /> : null;
 }
