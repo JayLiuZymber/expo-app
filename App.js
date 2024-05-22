d749f16120925bbb04c8a4c6fa239b8ee7130233
Jay <jayliu@zymber.tech>
2024-05-22 14:36:35 +0800

    + Animation $ npx expo install react-native-reanimated

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 /* 
-Color themes - Expo Documentation
-https://docs.expo.dev/develop/user-interface/color-themes/
+Animation - Expo Documentation
+https://docs.expo.dev/develop/user-interface/animation/
  */
-import { Text, StyleSheet, View, useColorScheme } from 'react-native';
-import { StatusBar } from 'expo-status-bar'; // Automatically switches bar style based on theme.
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from 'react-native-reanimated';
+import { View, Button, StyleSheet } from 'react-native';
 
-export default function App() {
-  const colorScheme = useColorScheme();
+export default function AnimatedStyleUpdateExample() {
+  const randomWidth = useSharedValue(10);
 
-  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
-  const themeContainerStyle =
-    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
 
   return (
-    <View style={[styles.container, themeContainerStyle]}>
-      <Text style={[styles.text, themeTextStyle]}>Color scheme: {colorScheme}</Text>
-      <StatusBar />
+    <View style={styles.container}>
+      <Animated.View style={[styles.box, style]} />
+      <Button
+        title="toggle"
+        onPress={() => {
+          randomWidth.value = Math.random() * 350;
+        }}
+      />
     </View>
   );
 }
@@ -26,19 +43,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    fontSize: 20,
-  },
-  lightContainer: {
-    backgroundColor: '#d0d0c0',
-  },
-  darkContainer: {
-    backgroundColor: '#242c40',
-  },
-  lightThemeText: {
-    color: '#242c40',
-  },
-  darkThemeText: {
-    color: '#d0d0c0',
+  box: {
+    width: 100,
+    height: 80,
+    backgroundColor: 'black',
+    margin: 30,
   },
 });
